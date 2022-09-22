7554d745ffaee17c9aeb13070f6509ae2688a131
DevamUdani3333 <103504908+DevamUdani3333@users.noreply.github.com>
2022-09-22 20:34:07 +0530

    Add files via upload

---
+++ Unit - 3/P6.java
@@ -0,0 +1,30 @@
+/* Write a Java program to demonstrate use of final keyword with class 
+and method.
+*/
+
+final class example
+{
+    int num;
+    
+    final void input()
+    {
+        num=143;
+    }
+
+    void display()
+    {
+        System.out.println(num);
+    }
+}
+
+   
+public class P6
+{
+    public static void main (String[] args) 
+    {
+        example e = new example();
+        
+        e.input();
+        e.display();
+    }
+}
